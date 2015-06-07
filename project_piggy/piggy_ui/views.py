40e1b0df3848af8c72dc32339297b9d2163cacd0
Charan <charanwordpress@gmail.com>
2015-06-07 05:16:25 -0500

    Added logo on first section

--- project_piggy/piggy_ui/views.py
+++ project_piggy/piggy_ui/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, render_to_response, RequestContext
 # Create your views here.
 
 def home(request):
-    return render_to_response("base_piggy_page.html", locals(), context_instance=RequestContext(request))
+    return render_to_response("piggy_page_with_elements.html", locals(), context_instance=RequestContext(request))
